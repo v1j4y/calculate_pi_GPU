0d61d4ad5d330b4647ba9ada9522d305baf92b0f
vijay gopal chilkuri <vijay.gopal.c@gmail.com>
2020-08-14 00:18:24 +0200

    include guard.

--- lib/parallel_reduction.h
+++ lib/parallel_reduction.h
@@ -3,7 +3,11 @@
  * Email:   vijay.gopal.c@gmail.com
  * Date:    12-08-2020
  */
+#ifndef PARALLEL_REDUCTION_H
+#define PARALLEL_REDUCTION_H
 
 #include "linear_algebra_helpers.h"
 
 int parallel_reduction(void);
+
+#endif // PARALLEL_REDUCTION_H
