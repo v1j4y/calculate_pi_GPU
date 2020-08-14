85b925d316c7aabc85197328578ac22d54a24c8c
vijay gopal chilkuri <vijay.gopal.c@gmail.com>
2020-08-14 20:02:25 +0200

    added testing header.

---
+++ lib/testing.h
@@ -0,0 +1,14 @@
+/*
+ * Author:  Vijay Gopal Chilkuri
+ * Email:   vijay.gopal.c@gmail.com
+ * Date:    12-08-2020
+ */
+#ifndef PARALLEL_REDUCTION_H
+#define PARALLEL_REDUCTION_H
+
+#include "linear_algebra_helpers.h"
+
+int testing(void);
+
+#endif // PARALLEL_REDUCTION_H
+
