74a3b70d37c0bee9a596887191cd45b3d147f358
vijay gopal chilkuri <vijay.gopal.c@gmail.com>
2020-08-21 01:39:18 +0200

    Level 2 optimization.

--- src/parallel_reduction.cu
+++ src/parallel_reduction.cu
@@ -54,6 +54,51 @@ __global__  void vectorReduction(unsigned int startIdx, Vector g_idata, Vector g
     if(tid == 0) g_odata.elements[blockIdx.x] = sdata[0];
 }
 
+// ----------------------------------------------------
+// Parallel reduction on GPU based on presentation
+// by Mark Harris, NVIDIA.
+// 
+// 
+// Kernel Optimization Level 2.
+//
+// ----------------------------------------------------
+__global__  void vectorReduction2(unsigned int startIdx, Vector g_idata, Vector g_odata){
+
+    // Size automatically determined using third execution control parameter
+    // when kernel is invoked.
+    extern __shared__ float sdata[];
+
+    int tid     = threadIdx.x;
+    int index   = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // This instruction copies data from 
+    // global to shared memory of each block.
+    // Only threads of a block can access this shared memory.
+    sdata[tid]  = g_idata.elements[startIdx + index];
+
+    // Synchronize threads, basically a barrier.
+    __syncthreads();
+    
+    // Do the reduction in shared memory buffer
+    // Thread Id:  0 - 1 - 2 - 3 - 4 - 5
+    //             |  /    |  /    |  /
+    //             0       2       4
+    for(unsigned int s = 1; s < blockDim.x; s *= 2)
+    {
+        int index = 2 * s * tid;
+
+        if (index < blockDim.x) 
+        { 
+          sdata[index] += sdata[index + s];
+        }
+    }
+
+    __syncthreads();
+
+    // Write back result to global memory
+    if(tid == 0) g_odata.elements[blockIdx.x] = sdata[0];
+}
+
 int parallel_reduction(void) 
 {
     int i,j;
@@ -137,7 +182,7 @@ int parallel_reduction(void)
       // Dimension of each block
       dim3 dimBlock(NThreadsPerBlock);
 
-      vectorReduction<<<dimGrid, dimBlock, NBdim>>>((idxParts - 1) * dimVec, Vinp_d, Vout_d);
+      vectorReduction2<<<dimGrid, dimBlock, NBdim>>>((idxParts - 1) * dimVec, Vinp_d, Vout_d);
 
 
       //--------------------------------------------------------
@@ -161,7 +206,7 @@ int parallel_reduction(void)
       if(idxParts == 1) Vout1 = AllocateZeroVector(dimOutVec);
       if(idxParts == 1) Vout1_d     = AllocateDeviceVector(Vout1);
 
-      vectorReduction<<<dimGrid1, dimBlock1, NBdim>>>(0, Vout_d, Vout1_d);
+      vectorReduction2<<<dimGrid1, dimBlock1, NBdim>>>(0, Vout_d, Vout1_d);
 
       // Copy data from device
       CopyFromDeviceVector(Vout1, Vout1_d);
