f545ed206d04e324f10bd06bd5d2cf0f5e7f4c4f
vijay gopal chilkuri <vijay.gopal.c@gmail.com>
2020-08-21 00:56:56 +0200

    Testing.

--- src/parallel_reduction.cu
+++ src/parallel_reduction.cu
@@ -117,7 +117,7 @@ int parallel_reduction(void)
 
       int dimVec  = LenVec;
       int dimOutVec = dimVec/NBdim;
-      if(idxParts == 1) Vout = AllocateZeroVector(dimOutVec);
+      Vout = AllocateZeroVector(dimOutVec);
 
       //--------------------------------------------------------
       // First  level 0
@@ -155,8 +155,8 @@ int parallel_reduction(void)
       dim3 dimBlock1(NThreadsPerBlock);
 
       // Create device vectors
-      if(idxParts == 1) Vout1 = AllocateZeroVector(dimOutVec);
-      if(idxParts == 1) Vout1_d     = AllocateDeviceVector(Vout1);
+      Vout1 = AllocateZeroVector(dimOutVec);
+      Vout1_d     = AllocateDeviceVector(Vout1);
 
       vectorReduction<<<dimGrid1, dimBlock1, NBdim>>>(Vout_d, Vout1_d);
 
