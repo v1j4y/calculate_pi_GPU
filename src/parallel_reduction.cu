a360751c9dc296e6cf5af689ed4b4b89e4f441f4
vijay gopal chilkuri <vijay.gopal.c@gmail.com>
2020-08-21 01:21:20 +0200

    Testing.

--- src/parallel_reduction.cu
+++ src/parallel_reduction.cu
@@ -58,7 +58,7 @@ int parallel_reduction(void)
 {
     int i,j;
     // Allocate and initialize the matrices
-    int nParts = 1024;
+    int nParts = 4096;
     Vector  V     = AllocateVector(nParts * LenVec);
     printf("----------------------\n");
     printf("Total Vector Size = %d\n",nParts * LenVec);
